e0743492d979e7d380a50f5de4bbefb8de7f70c6
Brayan <brayantandap@gmail.com>
2024-07-11 21:35:32 -0500

    feat > scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
-
-function toggleMenu(){
+document.addEventListener("DOMContentLoaded", function() {
     const menu = document.getElementById("menu_small_screens");
 
-    if(menu.style.display === 'none' || menu.style.display === ''){
-        menu.style.display = 'flex';
-    }else{
-        menu.style.display = 'none';
-    }
-    console.log('execute')
-}
+    // Asegura que el menú esté oculto inicialmente
+    menu.style.display = 'none'; 
+
+    // Función para alternar el menú
+    window.toggleMenu = function() {
+        if (menu.style.display === 'none' || menu.style.display === '') {
+            menu.style.display = 'flex';
+        } else {
+            menu.style.display = 'none';
+        }
+        console.log('execute');
+    };
+
+    // Oculta el menú cuando la ventana es redimensionada a una mayor resolución
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 800) {
+            menu.style.display = 'none';
+        }
+    });
+});
